e3167b58a1a000553139f3378d0454d86077067c
Raviteja <u2teja95@gmail.com>
2020-06-05 13:47:27 +0530

    commit with connectors

--- src/main/java/com/kafka/connect/rabbitmq/CamelRabbitmqSinkTask.java
+++ src/main/java/com/kafka/connect/rabbitmq/CamelRabbitmqSinkTask.java
@@ -2,6 +2,7 @@ package com.kafka.connect.rabbitmq;
 
 import java.util.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.camel.CamelContext;
 import org.apache.camel.Exchange;
 import org.apache.camel.ProducerTemplate;
@@ -12,14 +13,12 @@ import org.apache.camel.kafkaconnector.CamelSinkConnectorConfig;
 import org.apache.camel.kafkaconnector.utils.CamelMainSupport;
 import org.apache.camel.kafkaconnector.utils.TaskHelper;
 import org.apache.camel.support.DefaultExchange;
-import org.apache.kafka.connect.data.Schema;
-import org.apache.kafka.connect.data.SchemaBuilder;
 import org.apache.kafka.connect.errors.ConnectException;
-import org.apache.kafka.connect.header.Header;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 
 public class CamelRabbitmqSinkTask extends SinkTask {
 
@@ -67,56 +66,51 @@ public class CamelRabbitmqSinkTask extends SinkTask {
     @Override
     public void put(Collection<SinkRecord> sinkRecords) {
         for (SinkRecord record : sinkRecords) {
+            ObjectMapper mapper = new ObjectMapper();
             LOG.info("record: " + record.value());
             try {
-//            CamelContext camelContext1 = new DefaultCamelContext();
-//            CamelMainSupport cms1 = new CamelMainSupport(actualProps, LOCAL_URL, "rabbitmq://localhost:5672/"+record.value(), null, null, camelContext1);
-//            ProducerTemplate producer1 = cms.createProducerTemplate();
-//            cms1.start();
-            Map<String, Object> headers = new HashMap<String, Object>();
-            producer.getCamelContext().getRoutes().forEach(r -> {
-                if (r.getId() != null) {
-                    try {
-                        producer.getCamelContext().stop();
-                        LOG.info("Stopped: ");
-                        boolean removed = producer.getCamelContext().removeRoute(r.getRouteId());
-                        LOG.info("Removed: " + removed);
-                    } catch (Exception e) {
-                        LOG.info("Removing reoute error");
-                    }
-                }
+                Output out = mapper.readValue(record.value().toString(), Output.class);
+//                CamelContext camelContext1 = new DefaultCamelContext();
+//                CamelMainSupport cms1 = new CamelMainSupport(actualProps, LOCAL_URL, "rabbitmq:"+record.value()+"?hostname=localhost&skipExchangeDeclare=true", null, null, camelContext1);
+//                ProducerTemplate producer1 = cms.createProducerTemplate();
+//                cms1.start();
+                configureRoute(record, out.getKey());
+                Map<String, Object> headers = new HashMap<String, Object>();
+                Exchange exchange = new DefaultExchange(producer.getCamelContext());
+                headers.put(KAFKA_RECORD_KEY_HEADER, record.key());
+                LOG.info("Header key:" + record.key());
+                exchange.getMessage().setHeaders(headers);
+                exchange.getMessage().setBody(out.getValue());
+                LOG.info("Sending {} to {}", exchange, LOCAL_URL);
+                producer.send(LOCAL_URL, exchange);
+                //cms.stop();
+            } catch (Exception e) {
+                throw new ConnectException("Failed to create and start Camel context", e);
             }
-            );
-            producer.getCamelContext().addRoutes(new RouteBuilder() {
-                public void configure() {
-                    from(LOCAL_URL)
-                            .to("rabbitmq://localhost:5672/"+record.value());
-                }
-            });
-            producer.getCamelContext().start();
-
-            Exchange exchange = new DefaultExchange(producer.getCamelContext());
-            LOG.info("exhange: " + exchange.getExchangeId() + exchange.getFromRouteId());
-            headers.put(KAFKA_RECORD_KEY_HEADER, record.key());
-            for (Iterator<Header> iterator = record.headers().iterator(); iterator.hasNext();) {
-                Header header = (Header)iterator.next();
-                if (header.key().startsWith(HEADER_CAMEL_PREFIX)) {
-                    addHeader(headers, header);
-                } else if (header.key().startsWith(PROPERTY_CAMEL_PREFIX)) {
-                    addProperty(exchange, header);
-                }
-            }
-            LOG.info("headers: " + headers);
-            exchange.getMessage().setHeaders(headers);
-            exchange.getMessage().setBody(record.value());
-            LOG.info("Sending {} to {}", exchange, LOCAL_URL);
-            producer.send(LOCAL_URL, exchange);
-            //cms1.stop();
-        } catch (Exception e) {
-            throw new ConnectException("Failed to create and start Camel context", e);
-        }
 
         }
+    }
+
+    private void configureRoute(SinkRecord record, String exchange) throws Exception {
+        producer.getCamelContext().getRoutes().forEach(r -> {
+            if (r.getId() != null) {
+                try {
+                    producer.getCamelContext().stop();
+                    producer.getCamelContext().removeRoute(r.getRouteId());
+                    LOG.info("Removed route");
+                } catch (Exception e) {
+                    LOG.info("Removing reoute error");
+                }
+            }
+        });
+        producer.getCamelContext().addRoutes(new RouteBuilder() {
+            public void configure() {
+                from(LOCAL_URL)
+                        .to("rabbitmq://localhost:5672/"+exchange);
+            }
+        });
+        producer.getCamelContext().start();
+        producer = cms.createProducerTemplate();
     }
 
     CamelSinkConnectorConfig getCamelSinkConnectorConfig(
@@ -143,59 +137,6 @@ public class CamelRabbitmqSinkTask extends SinkTask {
         }
     }
 
-    private void addHeader(Map<String, Object> map, Header singleHeader) {
-        Schema schema = singleHeader.schema();
-        if (schema.type().getName().equals(Schema.STRING_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), (String)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.BOOLEAN_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), (Boolean)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.INT32_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.BYTES_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), (byte[])singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.FLOAT32_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), (float)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.FLOAT64_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), (double)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.INT16_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), (short)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.INT64_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), (long)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.INT8_SCHEMA.type().getName())) {
-            map.put(singleHeader.key(), (byte)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(SchemaBuilder.map(Schema.STRING_SCHEMA, Schema.STRING_SCHEMA).type().getName())) {
-            map.put(singleHeader.key(), (Map<?, ?>)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(SchemaBuilder.array(Schema.STRING_SCHEMA).type().getName())) {
-            map.put(singleHeader.key(), (List<?>)singleHeader.value());
-        }
-    }
-
-    private void addProperty(Exchange exchange, Header singleHeader) {
-        Schema schema = singleHeader.schema();
-        if (schema.type().getName().equals(Schema.STRING_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (String)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.BOOLEAN_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (Boolean)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.INT32_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.BYTES_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (byte[])singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.FLOAT32_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (float)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.FLOAT64_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (double)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.INT16_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (short)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.INT64_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (long)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(Schema.INT8_SCHEMA.type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (byte)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(SchemaBuilder.map(Schema.STRING_SCHEMA, Schema.STRING_SCHEMA).type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (Map<?, ?>)singleHeader.value());
-        } else if (schema.type().getName().equalsIgnoreCase(SchemaBuilder.array(Schema.STRING_SCHEMA).type().getName())) {
-            exchange.getProperties().put(singleHeader.key(), (List<?>)singleHeader.value());
-        }
-    }
 
     public CamelMainSupport getCms() {
         return cms;
